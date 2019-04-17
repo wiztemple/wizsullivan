e1f00ecae881681e574c62e97aa225b6ff3e5cf9
wiztemple <wiztemple@gmail.com>
2019-04-17 13:57:21 +0100

    add a favicon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,16 @@
 /* navabar scroll */
-window.addEventListener('scroll', () => {
+const body = document.getElementById('wiz');
+const navScroll = document.getElementById('scrollNav');
+
+
+const changeNav = () => {
     const yscroll = window.scrollY;
-    const navScroll = document.getElementById('scrollNav');
     console.log(yscroll);
-    if (yscroll === '50px') {
+    if (yscroll === '100px') {
         navScroll.style.backgroundColor = '#fff';
     }
     return null;
-});
+}
+
+
+body.addEventListener('scroll', changeNav);
