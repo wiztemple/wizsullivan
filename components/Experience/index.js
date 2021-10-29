5065ddcf7ab68b1e59e7d31d22f171c236919467
wiztemple <wiztemple7@gmail.com>
2021-10-29 18:14:06 +0100

    add experience as a component

--- components/Experience/index.js
+++ components/Experience/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Experiencecard.module.css';
+import styles from './Experience.module.css';
 
 const Experience = ({time, jobTitle, company}) => {
   return (
